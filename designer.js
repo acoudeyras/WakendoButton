2377d9711adf7d2ba6155320552609b3c7541c37
Ricardo Mello <ricardo.mello@4d.com>
2015-04-09 14:55:49 +0200

    Adding setPanelStyle

--- designer.js
+++ designer.js
@@ -6,6 +6,16 @@
 	
 	KendoButton.addEvent({
 		'name':'click'
-	});	
+	});
+	
+	KendoButton.setPanelStyle({
+	        'fClass': true,
+	        'text': true,
+	        'background': true,
+	        'border': true,
+	        'sizePosition': true,
+	        'label': true,
+	        'disabled': ['border-radius']
+    	});
 
 });
